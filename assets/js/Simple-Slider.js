3425b93103646542410da5df0480764d04b88ee0
MiltMont <miltgonmon@pm.me>
2021-04-21 03:08:05 -0500

    Website update.

--- assets/js/Simple-Slider.js
+++ assets/js/Simple-Slider.js
@@ -3,12 +3,12 @@ $(function(){
     // Initializing the swiper plugin for the slider.
     // Read more here: http://idangero.us/swiper/api/
     
-    var mySwiper = new Swiper ('.swiper-container', {
+    const mySwiper = new Swiper ('.swiper-container', {
+        autoplay: 2500,
         loop: true,
         pagination: '.swiper-pagination',
         paginationClickable: true,
         nextButton: '.swiper-button-next',
         prevButton: '.swiper-button-prev'
-    });
-    
+    });    
 });
